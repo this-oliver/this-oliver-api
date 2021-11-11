0d91c400cd339a7cc17230a5be0f4dc5c4528512
olivermanzi <olivermanzi.dev@gmail.com>
2021-11-11 10:09:00 +0100

    adds colors to tags

--- src/models/tag.js
+++ src/models/tag.js
@@ -1,9 +1,13 @@
 // data
+const colorHelper = require("../helpers/color");
 const Mongoose = require("mongoose");
 const Schema = Mongoose.Schema;
 
 const tag = new Schema(
-	{ name: { type: String, required: true, unique: true } },
+	{
+		name: { type: String, required: true, unique: true },
+		color: { type: String, unique: true, default: colorHelper.getRandomColor({ light: true }) }
+	},
 	{ timestamps: true }
 );
 
