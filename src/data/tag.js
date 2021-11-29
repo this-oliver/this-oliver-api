8eadf7d600c79d24f43bdaddb7c72e54d2234d46
Oliver Manzi <olivermanzi.dev@gmail.com>
2021-11-29 12:45:42 +0100

    fixes tags

--- src/data/tag.js
+++ src/data/tag.js
@@ -9,7 +9,7 @@ exports.createTag = async (name) => {
 	} catch (error) {
 		throw {
 			status: 400,
-			message: error, 
+			message: error,
 		};
 	}
 };
@@ -17,7 +17,7 @@ exports.createTag = async (name) => {
 exports.indexTags = async (showSecrets = false) => {
 	try {
 		const tags = await TagSchema.find().exec();
-		
+
 		//! PATCH
 		for (let i = 0; i < tags.length; i++) {
 			const tag = tags[i];
@@ -26,7 +26,6 @@ exports.indexTags = async (showSecrets = false) => {
 				await tag.save();
 			}
 		}
-
 	} catch (error) {
 		throw {
 			status: 400,
@@ -34,24 +33,21 @@ exports.indexTags = async (showSecrets = false) => {
 		};
 	}
 
-
 	try {
-		if(showSecrets){
+		if (showSecrets) {
 			return await TagSchema.find().exec();
-		}
-		
-		else {
+		} else {
 			const publicTags = [];
 			const publicArticles = await ArticleSchema.find({ publish: true }).exec();
 			const tags = await TagSchema.find().exec();
 
-			for(let i = 0; i < tags.length; i++){
+			for (let i = 0; i < tags.length; i++) {
 				const tag = tags[i];
 
-				for(let x = 0; x < publicArticles.length; x++){
+				for (let x = 0; x < publicArticles.length; x++) {
 					const article = publicArticles[x];
 
-					if(article.tags.includes(tag._id)){
+					if (article.tags.includes(tag._id)) {
 						publicTags.push(tag);
 						break;
 					}
@@ -63,7 +59,7 @@ exports.indexTags = async (showSecrets = false) => {
 	} catch (error) {
 		throw {
 			status: 400,
-			message: error, 
+			message: error,
 		};
 	}
 };
@@ -72,7 +68,7 @@ exports.getTag = async (id) => {
 	if (!id) {
 		throw {
 			status: 400,
-			message: "missing id", 
+			message: "missing id",
 		};
 	}
 
@@ -82,7 +78,7 @@ exports.getTag = async (id) => {
 	} catch (error) {
 		throw {
 			status: 400,
-			message: error, 
+			message: error,
 		};
 	}
 };
@@ -96,7 +92,7 @@ exports.deleteTag = async (id) => {
 	} catch (error) {
 		throw {
 			status: error.status || 400,
-			message: error.message || error, 
+			message: error.message || error,
 		};
 	}
 };
@@ -108,7 +104,7 @@ exports.cleanTags = async (dirtyList) => {
 
 	try {
 		const cleanList = [];
-		const allTags = await this.indexTags();
+		const allTags = await this.indexTags(true);
 
 		for (let x = 0; x < dirtyList.length; x++) {
 			const dirtyTag = dirtyList[x];
@@ -117,7 +113,8 @@ exports.cleanTags = async (dirtyList) => {
 			// check if tag exists
 			for (let i = 0; i < allTags.length; i++) {
 				const cleanTag = allTags[i];
-				if (dirtyTag.toLowerCase() == cleanTag.name.toLowerCase()) {
+
+				if (dirtyTag.name.toLowerCase() == cleanTag.name.toLowerCase()) {
 					found = true;
 					cleanList.push(cleanTag._id);
 					break;
@@ -126,7 +123,7 @@ exports.cleanTags = async (dirtyList) => {
 
 			// if tag is not found, add it to list
 			if (!found) {
-				const newTag = await this.createTag(dirtyTag);
+				const newTag = await this.createTag(dirtyTag.name);
 				cleanList.push(newTag._id);
 			}
 		}
@@ -135,7 +132,7 @@ exports.cleanTags = async (dirtyList) => {
 	} catch (error) {
 		throw {
 			status: error.status || 400,
-			message: error.message, 
+			message: error.message,
 		};
 	}
 };
